d5cfae2f61ae42266a482e623e3698ad49c734c7
TcePrepK <a.s.arda.simsek@gmail.com>
2025-12-04 12:23:25 +0300

    Post functions

---
+++ services/post/migrations/001_post_funcs.up.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION post.create_post(
+    new_user_id UUID,
+    new_post_content TEXT,
+    new_image_url TEXT
+)
+    RETURNS TABLE
+            (
+                user_id      UUID,
+                post_content TEXT,
+                image_url    TEXT,
+                created_at   TIMESTAMP
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    -- Insert the new post and return the inserted row
+    RETURN QUERY
+        INSERT INTO post.posts (userid, content, image_url)
+            VALUES (new_user_id, new_post_content, new_image_url)
+            RETURNING user_id, content, image_url, created_at;
+END;
+$$;
