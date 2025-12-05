33228b5712b30f875171e571739fdbe9808dd52d
TcePrepK <a.s.arda.simsek@gmail.com>
2025-12-05 04:23:44 +0300

    Merged 518da0dec035d86a5ee74f93918049a816fb597c

---
+++ services/post/src/db/post.rs
@@ -0,0 +1,35 @@
+use sqlx::PgPool;
+use utils::db_call;
+use utils::errors::AuthError;
+use uuid::Uuid;
+
+pub struct PostHandler {}
+
+impl PostHandler {
+    /// Tries to create a post in the database.
+    ///
+    /// # Arguments
+    /// * `pool` - A reference to a database connection pool.
+    /// * `username` - The username of the new account.
+    /// * 'content' - Content of the post
+    /// * 'image url' - Url of the post image
+    ///
+    ///
+    /// # Returns
+    /// A tuple containing the post ID and creation date.
+    ///
+    /// # Errors
+    /// * `AuthError::Db` - If there was an unexpected error!
+    pub async fn create_post(
+        pool: &PgPool,
+        user_id: Uuid,
+        post_content: &str,
+        image_url: &str,
+    ) -> Result<(Uuid, String), AuthError> {
+        db_call!(
+            pool = pool,
+            query = sqlx::query_as(r#"SELECT created_at FROM post.create_post($1, $2, $3)"#),
+            binds = [user_id, post_content, image_url]
+        )
+    }
+}
