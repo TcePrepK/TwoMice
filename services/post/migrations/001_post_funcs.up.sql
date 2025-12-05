6cf64de9c6b4f45108555a2f2520e8e84af715d6
TcePrepK <a.s.arda.simsek@gmail.com>
2025-12-06 01:26:38 +0300

    post: Fixed create_post function

--- services/post/migrations/001_post_funcs.up.sql
+++ services/post/migrations/001_post_funcs.up.sql
@@ -1,34 +1,34 @@
-CREATE OR REPLACE FUNCTION post.create_post(
+CREATE OR REPLACE FUNCTION create_post(
     token TEXT,
     new_post_content TEXT,
     new_image_url TEXT
 )
     RETURNS TABLE
             (
-                user_id      UUID,
-                post_content TEXT,
-                image_url    TEXT,
-                created_at   TIMESTAMP
+                user_id    UUID,
+                content    TEXT,
+                image_url  TEXT,
+                created_at TIMESTAMP
             )
     LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    uid UUID;
+    existing_user_id UUID;
 BEGIN
-    SELECT user_id
-    INTO uid
+    SELECT account_id
+    INTO existing_user_id
     FROM auth.sessions
     WHERE session_token = token;
 
-    IF uid IS NULL THEN
+    IF existing_user_id IS NULL THEN
         RAISE EXCEPTION 'Invalid token' USING ERRCODE = '23502';
     END IF;
 
     -- Insert new post and return full row
     RETURN QUERY
-        INSERT INTO post.posts (userid, content, image_url)
-            VALUES (uid, new_post_content, new_image_url)
-            RETURNING userid, content, image_url, created_at;
+        INSERT INTO posts as p (user_id, content, image_url)
+            VALUES (existing_user_id, new_post_content, new_image_url)
+            RETURNING p.user_id, p.content, p.image_url, p.created_at;
 END;
 $$;
